f16e58e9ace6b5b4f93404a9e69e227c47475441
Attila Gulyas <toraritte@gmail.com>
2018-03-27 22:24:16 -0700

    Add solution (chapter 11, bintree builder)

---
+++ toraritte/chapter-11-binary-tree-builder.hs
@@ -0,0 +1,49 @@
+module BinTreeUnfoldr where
+
+data BinaryTree a =
+    Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Ord)
+
+instance (Num a, Eq a, Show a) => Show (BinaryTree a) where
+  show Leaf = "Leaf"
+  show (Node Leaf a Leaf)  = "Node Leaf " ++ (show a) ++ " Leaf"
+  show (Node left a right) =
+       "Node (" ++ (show left) ++ ")"
+    ++ "\n" ++ (padding $ a+1) ++ (show a)
+    ++ "\n" ++ (padding $ a+1) ++ "(" ++ (show right) ++ ")"
+
+-- padding :: Integer ->  String
+padding 1 = "     "
+padding n = "      " ++ (padding $ n-1)
+
+
+-- 1. Write unfold for BinaryTree.
+----------------------------------
+unfold :: (a -> Maybe (a,b,a)) -> a -> BinaryTree b
+unfold f a =
+  case (f a) of
+    Nothing ->
+      Leaf
+    Just (t, u, v) ->
+      Node (unfold f t) u (unfold f v)
+
+-- Basically this is exercise 2. goal, but that needs both branches
+-- to look the same. This is just a demonstration that they can be
+-- different.
+testUnfold :: Int -> BinaryTree String
+testUnfold = unfold go
+  where
+    go n
+     | n > 7     = Nothing
+     | otherwise = Just (n+2, (show n), n*2)
+
+-- 2. Make a tree builder.
+--------------------------
+-- See custom Show instance for BinaryTree on top.
+treeBuild :: Integer -> BinaryTree Integer
+treeBuild i = unfold go 0
+  where
+    go n
+     | n == i    = Nothing
+     | otherwise = Just (n+1, n, n+1)
